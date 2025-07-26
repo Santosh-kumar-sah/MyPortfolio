e638c110bfdeb09aca503feb4cda4990cf01b1ec
Santosh-Kumar-Sah <sureshsirf886@gmail.com>
2025-07-26 19:54:16 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Typing Effect
+const typed = new Typed(".typed-text", {
+  strings: ["Santosh Kumar Sah", "a Web Developer", "a CSE(AIML) Student"],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
+
+// Scroll-to-top button
+const scrollBtn = document.getElementById("scrollTopBtn");
+window.onscroll = function () {
+  scrollBtn.style.display = window.scrollY > 200 ? "block" : "none";
+};
+scrollBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
